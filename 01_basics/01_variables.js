c8f3d333aebc23264f0b560b9d41b2d022ee3138
Devangpareek <115179700+Devangpareek@users.noreply.github.com>
2024-09-13 04:34:01 +0000

    conversion operation

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -17,5 +17,5 @@ console.log(accountId);
 Prefer not to use var
 because of issue in block scope and functional scope
 */
-
+console.log(typeof accountCity)
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
